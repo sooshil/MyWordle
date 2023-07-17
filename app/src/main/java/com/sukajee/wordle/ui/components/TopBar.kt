f0baf70043f050d5093e6398aea1d412f9ef5a8c
sooshil <sushilkaf@gmail.com>
2023-07-17 19:41:49 -0400

    release update

--- app/src/main/java/com/sukajee/wordle/ui/components/TopBar.kt
+++ app/src/main/java/com/sukajee/wordle/ui/components/TopBar.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sukajee.wordle.R
@@ -45,7 +46,9 @@ fun TopBar(
             ) {
                 Text(
                     text = title,
-                    style = MaterialTheme.typography.bodyMedium,
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        fontWeight = FontWeight.Bold
+                    ),
                     fontSize = 18.sp
                 )
             }
